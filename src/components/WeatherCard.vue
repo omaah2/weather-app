421d3cc5efacc94ebdcc1c69645950b5b6f1ac91
omaah2 <ummkhaladoluwatosin@gmail.com>
2023-12-01 21:25:02 +0100

    ..

--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -19,16 +19,16 @@ const removePlace = (placeName) => {
 </script>
 
 <template>
-  <div :class="place.current.is_day === 1 ?'bg-day' :'bg-night' " class="text-white p-10 rounded-lg shadow-lg gap-6 mb-6 relative overflow-hidden ">
+  <div :class="place.current.is_day === 1 ? 'bg-day' : 'bg-night'" class="text-white p-4 md:p-6 lg:p-8 rounded-lg shadow-lg gap-4 md:gap-6 mb-6 relative overflow-hidden sm:w-72">
     <!-- Location & time -->
-    <div class="mb-2 flex justify-between items-center">
+    <div class="mb-2 flex flex-col md:flex-row  justify-between items-center">
       <div class="flex items-center justify-center gap-2">
         <i class="fa-solid fa-location-dot"></i>
-        <h1 class="text-3xl">{{ place.location.name }}</h1>
+        <h1 class="text-lg md:text-xl lg:text-2xl">{{ place.location.name }}</h1>
       </div>
       <div class="flex items-center justify-center gap-2">
         <i class="fa-solid fa-clock"></i>
-        <h1 class="text-3xl">
+        <h1 class="text-lg md:text-xl lg:text-2xl">
           {{ new Date(place.location.localtime).getHours() }}:
           {{ new Date(place.location.localtime).getMinutes() }}
         </h1>
@@ -36,10 +36,10 @@ const removePlace = (placeName) => {
     </div>
 
     <!-- current weather -->
-    <div class="text-center flex-1">
-      <img :src="place.current.condition.icon" alt="icon" width="200" class="mx-auto -mb-10" />
-      <h1 class="text-9xl mb-2 -mr-4">{{ Math.round(place.current.temp_c) }}&deg;</h1>
-      <p class="text-2xl">{{ place.current.condition.text }}</p>
+    <div class="text-center">
+      <img :src="place.current.condition.icon" alt="icon" class="w-16 md:w-20 lg:w-24 mx-auto -mb-4 md:-mb-6 lg:-mb-8" />
+      <h1 class="text-4xl md:text-5xl lg:text-7xl mb-2">{{ Math.round(place.current.temp_c) }}&deg;</h1>
+      <p class="text-base md:text-lg lg:text-xl">{{ place.current.condition.text }}</p>
     </div>
 
     <BorderLine />
@@ -52,24 +52,25 @@ const removePlace = (placeName) => {
     <!-- info -->
     <Transition name="fade">
       <div v-show="showDetail">
-      <WeatherInfo
-        :place="place"
-        @close-info="showDetail = false"
-        @remove-place="removePlace(place.location.name)"
-      />
-    </div>
+        <WeatherInfo
+          :place="place"
+          @close-info="showDetail = false"
+          @remove-place="removePlace(place.location.name)"
+        />
+      </div>
     </Transition>
 
     <!-- forecast btn -->
-    <div class="flex justify-end items-center gap-1 mt-10">
+    <div class="flex justify-center md:justify-end items-center gap-1 mt-4 md:mt-6 lg:mt-8">
       <button @click="showDetail = true">
-        More <i class="fa-solid fa-arrow-right text-sm -mb-px"></i>
+        More <i class="fa-solid fa-arrow-right text-xs md:text-sm -mb-px"></i>
       </button>
     </div>
   </div>
 </template>
 
 <style scoped>
+
 .bg-day{
   background-color: #8ec5fc;
   background-image: linear-gradient(62deg, #8ec5fc 0%,#e0c3fc 100% );
@@ -87,4 +88,10 @@ const removePlace = (placeName) => {
 .fade-leave-to {
   opacity: 0;
 }
+
+
+/* You can add further media queries for different screen sizes */
 </style>
+
+
+
--- src/components/WeatherInfo.vue
+++ src/components/WeatherInfo.vue
@@ -1,66 +1,88 @@
 <script setup>
-defineProps({
-  place: Object
-})
+  defineProps({
+    place: Object
+  })
 </script>
 
 <template>
-  <div
-    class="absolute bg-white/80 backdrop-blur-sm text-slate-900 inset-x-0 bottom-0 rounded-t-lg p-8"
-  >
+  <div class=" absolute bg-white/80 backdrop-blur-sm text-slate-900 inset-x-0 bottom-0 rounded-t-lg p-8">
     <!-- Close button -->
-    <div class="flex justify-end mb-10">
-      <button  @click="$emit('close-info')" class="p-1">
+    <div class="flex justify-end mb-6 lg:mb-10 sm:mb-4">
+      <button @click="$emit('close-info')" class="p-1">
         <i class="fa-solid fa-xmark text-xl"></i>
       </button>
     </div>
 
-    <div class="flex items-center justify-between gap-6 mb-20">
+    <div class="grid grid-cols-1 lg:grid-cols-3 gap-6 lg:gap-10 sm:gap-4">
       <!-- Wind speed -->
-      <div class="text-center flex-1">
+      <div class="text-center">
         <i class="fa-solid fa-wind mb text-2xl"></i>
         <p class="text-xl font-bold">{{ place.current.wind_kph }} km/h</p>
         <p>wind</p>
       </div>
       <!-- Humidity level -->
-      <div class="text-center flex-1">
+      <div class="text-center">
         <i class="fa-solid fa-droplet mb text-2xl"></i>
         <p class="text-xl font-bold">{{ place.current.humidity }}%</p>
         <p>humidity</p>
       </div>
       <!-- Precipitation -->
-      <div class="text-center flex-1">
+      <div class="text-center">
         <i class="fa-solid fa-umbrella mb text-2xl"></i>
         <p class="text-xl font-bold">{{ place.current.precip_mm }}mm</p>
         <p>precipitation</p>
       </div>
-    </div>
-    <div class="flex items-center justify-between gap-6 mb-10">
       <!-- Wind direction -->
-      <div class="text-center flex-1">
+      <div class="text-center">
         <i class="fa-solid fa-fan mb text-2xl"></i>
         <p class="text-xl font-bold">{{ place.current.wind_dir }}</p>
         <p>direction</p>
       </div>
       <!-- Feels like -->
-      <div class="text-center flex-1">
+      <div class="text-center">
         <i class="fa-solid fa-temperature-half mb text-2xl"></i>
         <p class="text-xl font-bold">{{ Math.round(place.current.feelslike_c) }}</p>
         <p>Feels</p>
       </div>
       <!-- UV -->
-      <div class="text-center flex-1">
+      <div class="text-center">
         <i class="fa-solid fa-sun mb text-2xl"></i>
         <p class="text-xl font-bold">{{ place.current.uv }}</p>
         <p>UV index</p>
       </div>
     </div>
     <!-- Last update and delete -->
-    <div class="flex justify-between items-center">
-      <h3 class="text-slate-900/50">last update:{{ place.current.last_updated }}</h3>
+    <div class="flex justify-between items-center mt-6 lg:mt-10">
+      <h3 class="text-slate-900/50 text-sm lg:text-base">Last update: {{ place.current.last_updated }}</h3>
       <button @click="$emit('remove-place')">
         <i class="fa-solid fa-trash"></i>
       </button>
     </div>
   </div>
 </template>
+
+<style scoped>
+/* You can add any additional scoped styles here */
+@media screen and (max-width: 639px) {
+  .absolute {
+    scale: 0.9;
+    top: 10px;
+    padding: 0;
+    
+  }
+  .text-center{
+    font-size: 14px;
+  }
+  .text-xl{
+    font-size: 16px;
+  }
+  .grid{
+
+  }
+}
+</style>
+
+
+
+
+
